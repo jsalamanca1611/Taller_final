f2102f3aa291949d7d6a886e60352858c4deda08
Juan Pablo <jsalamancam@unbosque.edu.co>
2026-04-13 08:11:32 -0500

    Add js

---
+++ Taller.js
@@ -0,0 +1,21 @@
+//Datos básicos
+
+let nombreCompleto = "Juan Caballo Loco";
+let edad = 0;
+let tipoDocumento = "CC";
+let numeroDocumento = "1028863980";
+
+//Validación de perfil
+
+if (edad < 18) 
+if (edad > 17 & <25)
+if (edad >24 & <60)
+if (edad >59)    
+
+
+//Información salarial
+
+let salarioSueldo = 0;  
+let comisiones = 0;          
+let totalHorasExtra = 0;      
+let nivelRiesgo = "";       
